ec1375f830012a953ea001b05cb305ed104ef106
RenanRodrigo <dantasrenan292@gmail.com>
2026-05-21 19:08:38 -0300

    ap.git

--- Inimigos.py
+++ Inimigos.py
@@ -1,12 +1,28 @@
+import pygame
+
+
 class inimigos:
-def __init__(self, x, y):
+  def __init__(self, x, y, velocidade = 2):
     self.y = y
     self.x = x
+    self.velocidade = velocidade
 
-def perseguir(self,player):
+
+  def perseguir(self,player):
+    if self.x < player.posx:
+      self.x += self.velocidade
+
+    elif self.x > player.posx:
+      self.x -= self.velocidade
+
+  def desenhar(self, tela):
+      
+      pygame.draw.rect(tela,(234,109,0), (int(self.x), int(self.y), 40, 40))
+
     
-class aereo:
-
-class parado:
-
-class terrestre:
+class aereo(inimigos):
+ pass
+class parado(inimigos):
+ pass
+class terrestre(inimigos):
+  pass
