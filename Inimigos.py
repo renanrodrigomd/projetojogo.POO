3f7eabc256d797b90d297feff877b414b47a5c29
RenanRodrigo <dantasrenan292@gmail.com>
2026-05-21 18:33:40 -0300

    atu.py

--- Inimigos.py
+++ Inimigos.py
@@ -1,1 +1,12 @@
 class inimigos:
+def __init__(self, x, y):
+    self.y = y
+    self.x = x
+
+def perseguir(self,player):
+    
+class aereo:
+
+class parado:
+
+class terrestre:
--- Player.py
+++ Player.py
@@ -1,1 +1,23 @@
+import pygame
+import math
+
 class player:
+    def __init__(self, y, x):
+        self.posy = y
+        self.posx = x
+        self.direção = 'd'
+
+    def mover(self, direction):
+        self.direção = direction
+
+        if direction == "d":
+           self.posx -= 2
+        elif direction == "a":
+           self.posx += 2
+        elif direction == "space":
+           self.posy -= 6
+
+
+
+    def desenhar(self, tela):
+        pygame.draw.rect(tela,(255,167,0), (int(self.posx), tela.get_height() -40, 40, 40))
